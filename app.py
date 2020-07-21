3dc075b2afd10bebb6bab99d9f2ae78e8a4799ef
ugneokmanaite <uokmanaite@spartaglobal.com>
2020-07-21 15:08:47 +0100

    exercise uploaded- needs doing

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # first we need to import flask
-from flask import Flask
+# import render template for rendering
+from flask import Flask, render_template, url_for, request
 
 # in order for us to use flask we need to create an instance of our app
 # creating a flask instance
@@ -10,8 +11,20 @@ app = Flask(__name__)
 # block of code for default page
 @app.route("/")
 # create a welcome method to display on homepage
-def index():
-    return "<h1>Welcome to MVC with flask project</h1>"
+# def index():
+    # return "<h1>Welcome to MVC with flask project</h1>"
+def welcome_user():
+    return render_template("base.html")
+
+@app.route("/login/")
+def login_user():
+    return render_template("index.html")
+
+
+# login functionality with GET, POST methods of HTTP
+# import requests to use the methods check status code
+# add control flow to redirect the user according to their status code received
+
 
 
 # index method will be called at the endpoint
@@ -30,9 +43,10 @@ if __name__ == "__main__":
 # in the browser when we load the page from home page to user name i.e your name
 # it should display your name in the browser with message from your methods
 
-@app.route("/<name>")
-def welcome_user(name):
-    return f"<h1>Welcome to Python flask app dear {name}</h1>"
+# @app.route("/<username>")
+# def welcome_user(username):
+#     # return f"<h1>Welcome to Python flask app dear {username}</h1>"
+#     return render_template("index.html")
 
-# if __name__ == "__main__":
-#     app.run(debug=True)
+# when a error/exception appears - inspect line number at end of error & review your code
+# when a error/exception appears - inspect line number at end of error in the browser & review your code
